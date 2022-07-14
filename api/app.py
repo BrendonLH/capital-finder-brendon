92a28846e3b1e1ae614fd3b76457b54297092989
Brendon <brendonhampton1988@gmail.com>
2022-07-14 00:12:02 -0700

    got the request to hit properly, next step is to get the data properly parsed for what I need

--- api/app.py
+++ api/app.py
@@ -1,12 +1,33 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
-
     def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        print(url_components)
+        dic = dict(query_string_list)
+        print(dic)
+
+        if "country" in dic:
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + dic["country"])
+            print(r, 'this is in the response')
+            data = r.json()
+            print(data)
+            country = []
+            message = "hitting the get request"
+
+        else:
+            message = "Select A Country or A Capital"
+
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        message = "Hello World!."
+
         self.wfile.write(message.encode())
+
         return
